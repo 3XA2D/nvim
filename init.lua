9bf6cb4a5af18fd6b9509cc17f17a8b7e3aae820
3XA2D <fstar2512@gmail.com>
2023-02-28 13:34:43 +0700

    Change indent from > -> |

--- init.lua
+++ init.lua
@@ -194,7 +194,7 @@ require('Comment').setup()
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 require('indent_blankline').setup {
-  char = '>',
+  char = '|',
   show_trailing_blankline_indent = false,
 }
 
